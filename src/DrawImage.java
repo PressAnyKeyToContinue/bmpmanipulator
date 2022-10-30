53a60bf4219c3f00e1aaf1e7de9f3630ad32367a
PressAnyKey <macekev@gmail.com>
2022-10-30 17:02:26 +0100

    Added two windows for before and after and added a try catch to allow program execution even after the thrown exception.

--- src/DrawImage.java
+++ src/DrawImage.java
@@ -9,9 +9,12 @@ public class DrawImage {
     public static void main(String[] avg) throws IOException
     {
         //C:\Users\macek\IdeaProjects\bmpeditor\sprites\sample.bmp
-        DrawImage di = new DrawImage(manipulator.manipulate(imageLoader.image("sample.bmp")));
+        BufferedImage img = imageLoader.image("sample.bmp");
+
+        DrawImage before = new DrawImage(img, "Before");
+        DrawImage di = new DrawImage(manipulator.manipulate(img), "After");
     }
-    public DrawImage(BufferedImage img) throws IOException
+    public DrawImage(BufferedImage img, String label) throws IOException
     {
         ImageIcon icon = new ImageIcon(img);
         JFrame frame = new JFrame();
@@ -19,6 +22,7 @@ public class DrawImage {
         frame.setSize(1800, 1000);
         JLabel lbl = new JLabel();
         lbl.setIcon(icon);
+        lbl.setText(label);
         frame.add(lbl);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/ManipulateImage.java
+++ src/ManipulateImage.java
@@ -1,20 +1,27 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
+
 
 public class ManipulateImage {
     //TODO: Fix ArrayIndexOutOfBoundsException
-    public static BufferedImage manipulate (BufferedImage img){
-        int width = img.getWidth();
-        int height = img.getHeight();
-
-        for (int hi = 0; hi < height; hi++)
-        {
-            for (int wi = 0; wi < width; wi++)
-            {
-                int currentPixel = img.getRGB(wi, hi);
-                System.out.println(currentPixel);
-                img.setRGB(hi, wi,(currentPixel * 2));
+    public static BufferedImage manipulate (BufferedImage imgb){
+        BufferedImage imga = imgb;
+        int width = imga.getWidth();
+        int height = imga.getHeight();
+        try {
+            for (int hi = 0; hi < height; hi++) {
+                for (int wi = 0; wi < width; wi++) {
+                    int currentPixel = imga.getRGB(wi, hi);
+                    System.out.println(currentPixel);
+                    System.out.println((int)(currentPixel * Math.sin((double)wi)));
+                    imga.setRGB(hi, wi, Color.BLUE.getRGB()/*(int) (currentPixel * Math.sin((double)wi))*/ );
+                }
             }
         }
-        return img;
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("\nCaught " + e);
+        }
+        return imga;
     }
 }
